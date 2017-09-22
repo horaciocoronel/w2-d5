65996140f8c0b94bd719e430db1528c0335f72ee
horaciocoronel <arielc@gmail.com>
2017-09-22 13:50:34 -0400

    Finish exercise 1.5 - complete

--- bankAccount.rb
+++ bankAccount.rb
@@ -4,6 +4,7 @@ class BankAccount
   def initialize(balance, interest_rate)
     @balance = balance
     @interest_rate = interest_rate
+    @gain_interest = 0
   end
 
   # Reader (Getter)
@@ -18,6 +19,18 @@ class BankAccount
   # Writer (Setter)
   def deposit(amount)
     @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
+  #Reader (Getter)
+  def gain_interest
+    @gain_interest = @balance * @interest_rate / 100
+  end
+
+  def balance_plus_interest
+    @balance += @gain_interest
   end
 
 end
--- exercise1.rb
+++ exercise1.rb
@@ -7,3 +7,8 @@ puts "John's account interest rate =  #{john.interest_rate}%"
 puts "John deposits $200"
 john.deposit(200)
 puts "New balance $#{john.balance}"
+puts "John decides to withdraw $50"
+john.withdraw(50)
+puts "New balance $#{john.balance}"
+puts "John earned interest to date = #{john.gain_interest}"
+puts "Balance + earned interest =  $#{john.balance_plus_interest}"
